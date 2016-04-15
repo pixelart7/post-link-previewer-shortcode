043ff111c94d5de0eb47df50e20fa3e9c3817ced
Chaiyapat Tantiworachot <sonkungz_on@hotmail.com>
2016-04-15 19:56:25 +0700

    Init commit, built for personal blog

---
+++ post-link-previewer-shortcode.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Post Link Previewer Shortcode
+Description: Show preview and a link to a post using shortcode
+Version:     0.0.1
+Author:      Chaiyapat Tantiworachot
+Author URI:  https://pix7.me/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+defined( 'ABSPATH' ) or die( '' );
+
+function post_link_previewer_styles() {
+    wp_register_style( 'post-link-previewer', plugins_url( 'post-link-previewer-shortcode/style.css' ) );
+    wp_enqueue_style( 'post-link-previewer' );
+}
+
+function post_link_previewer_do( $atts ){
+
+    $info_texts = array(
+        "en" => "This post is also available in English",
+        "th" => "อ่านโพสต์นี้ในภาษาไทย"
+    );
+
+    $a = shortcode_atts( array(
+        'id' => 0,
+        'to' => 'en',
+        'blank' => false,
+        'info' => 'PRESET'
+    ), $atts );
+    if( $a['id'] == 0 || get_post_status( $a['id'] ) === FALSE ) return;
+    $post = get_post( $a[ 'id' ] );
+    if( $post->post_status != 'publish' ) return;
+    $res = array(
+        'title' => apply_filters( 'the_title', $post->post_title ),
+        'content' => wp_strip_all_tags( apply_filters( 'the_content', $post->post_content ) ),
+        'hasImage' => has_post_thumbnail( $post->ID ),
+        'url' => get_permalink( $post->ID ),
+        'thumb' => ''
+    );
+    if($res['hasImage']) $res['thumb'] = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+
+    ?>
+        <a href='<?php echo $res['url']; ?>' class="post-link-previewer-anchor">
+            <div class="post-link-previewer">
+                <?php if( ! $a['blank'] ) { ?>
+                <div class="infos">
+                    <p class="info-text"><?php echo ($a['info'] == 'PRESET')? $info_texts[ $a['to'] ] . "&rarr;": $a['info']; ?></p>
+                </div>
+                <?php } ?>
+                <div class="link-preview">
+                    <?php if($res['hasImage']){ ?><div class="thumb" style="background-image: url('<?php echo $res['thumb']; ?>')"></div><?php } ?>
+                    <div class="content-wrapper">
+                        <div class="content">
+                            <p class="title"><?php echo $res['title'] ?></p>
+                            <p class="post-content"><?php echo $res['content'] ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    <?php
+
+    print_r($res);
+}
+
+add_action( 'wp_enqueue_scripts', 'post_link_previewer_styles' );
+
+add_shortcode( 'post-link-previewer', 'post_link_previewer_do' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+=== Post Link Previewer Shortcode ===
+Contributors: pixelart7
+Tested up to: 4.5
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Show preview and a link to a post using shortcode
